5c9402e8905e321574f9cf1b35347c5a114d9903
Weifeng Liu <weifeng.liu@intel.com>
2024-06-03 05:37:40 +0000

    drm/i915: Remove unused function in backlight setting
    intel_dp_ser_write_reg() is not used, so let's get rid of it.
    
    Test-done:
        Android boot tested with panel EF1E-A1
    
    Tracked-On: OAM-119963
    Tracked-On: OAM-119035
    Signed-off-by: Weifeng Liu <weifeng.liu@intel.com>

--- drivers/gpu/drm/i915/display/intel_dp.c
+++ drivers/gpu/drm/i915/display/intel_dp.c
@@ -5570,31 +5570,6 @@ bool intel_dp_mcu_write_reg(struct drm_device *dev, struct i2c_adapter *adapter,
 	return true;
 }
 
-bool intel_dp_ser_write_reg(struct drm_device *dev, struct i2c_adapter *adapter, unsigned int reg_addr, u8 val)
-{
-	int ret = 0;
-	struct i2c_msg msg;
-	u8 buf[2];
-
-	buf[0] = reg_addr & 0xff;
-	buf[1] = val;
-
-	msg.addr = 0x30;
-	msg.flags = 0;
-	msg.buf = &buf[0];
-	msg.len = 2;
-
-	ret = i2c_transfer(adapter, &msg, 1);
-	if (ret < 0) {
-		drm_err(dev, "[FPD_DP] [-%s-%s-%d-], fail client->addr=0x%02x, reg_addr=0x%02x, val=0x%02x\n",
-				__FILE__, __FUNCTION__, __LINE__, msg.addr, reg_addr, val);
-		return false;
-	}
-	drm_dbg_kms(dev, "[FPD_DP] WIB 984 reset0x%02x: 0x%02x 0x%02x OK\n",
-			msg.addr, reg_addr, val);
-	return true;
-}
-
 static u32 mcu_get_backlight(struct intel_connector *connector, enum pipe unused)
 {
 	struct intel_panel *panel = &connector->panel;
